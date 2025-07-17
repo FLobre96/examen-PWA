c5a36cd1885302453a138d9d37bbc6be54e11038
Facundo Lobrero <lobrerofacundo@yahoo.com.ar>
2025-07-17 16:33:13 -0300

    8vo

--- db.js
+++ db.js
@@ -1,19 +1,31 @@
-// Sección: IndexedDB
 let db;
 
 export function inicializarDB() {
-  const request = indexedDB.open("EncuestasCABA", 2);
-request.onupgradeneeded = function (event) {
-  db = event.target.result;
-  if (!db.objectStoreNames.contains("comentariosPendientes")) {
-    db.createObjectStore("comentariosPendientes", {
-      keyPath: "id",
-      autoIncrement: true
-    });
-  }
-};
-  request.onsuccess = e => { db = e.target.result; };
-} 
+  const request = indexedDB.open("EncuestasCABA", 1);
+
+  request.onupgradeneeded = e => {
+    db = e.target.result;
+    if (!db.objectStoreNames.contains("comentarios")) {
+      db.createObjectStore("comentarios", { keyPath: "id", autoIncrement: true });
+    }
+    if (!db.objectStoreNames.contains("comentariosPendientes")) {
+      db.createObjectStore("comentariosPendientes", { keyPath: "id", autoIncrement: true });
+    }
+  };
+
+  request.onsuccess = e => {
+    db = e.target.result;
+  };
+}
+
+export function guardarComentario(categoria, comentario) {
+  return new Promise(resolve => {
+    const tx = db.transaction("comentarios", "readwrite");
+    const store = tx.objectStore("comentarios");
+    store.add({ ...comentario, categoria });
+    tx.oncomplete = resolve;
+  });
+}
 
 export function guardarComentarioPendiente(categoria, comentario) {
   return new Promise(resolve => {
@@ -24,10 +36,10 @@ export function guardarComentarioPendiente(categoria, comentario) {
   });
 }
 
-export function revisarComentariosPendientes(categoria) {
+export function traerComentarios(categoria) {
   return new Promise(resolve => {
-    const tx = db.transaction("comentariosPendientes", "readonly");
-    const store = tx.objectStore("comentariosPendientes");
+    const tx = db.transaction("comentarios", "readonly");
+    const store = tx.objectStore("comentarios");
     const request = store.getAll();
     request.onsuccess = () => {
       resolve(request.result.filter(c => c.categoria === categoria));
@@ -35,7 +47,29 @@ export function revisarComentariosPendientes(categoria) {
   });
 }
 
-// Sección: Firebase Firestore
+export function reenviarComentariosPendientes() {
+  return new Promise(resolve => {
+    const tx = db.transaction("comentariosPendientes", "readwrite");
+    const store = tx.objectStore("comentariosPendientes");
+    const getAllRequest = store.getAll();
+
+    getAllRequest.onsuccess = async () => {
+      const pendientes = getAllRequest.result;
+
+      for (const comentario of pendientes) {
+        try {
+          await guardarComentario(comentario.categoria, comentario);
+          store.delete(comentario.id); 
+        } catch (e) {
+          console.error("Error reenviando comentario pendiente:", e);
+        }
+      }
+
+      resolve();
+    };
+  });
+}
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getFirestore,
